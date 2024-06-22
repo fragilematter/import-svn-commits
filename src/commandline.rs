301e644bd953dd9f6532949e5a2621ee30f25b1b
Doru Barbu <barbu.doru@gmail.com>
2024-06-22 13:59:43 +0300

    all command line arguments parsed

--- src/commandline.rs
+++ src/commandline.rs
@@ -1,10 +1,28 @@
 use clap::Parser;
 use url::Url;
+use email_address::EmailAddress;
+use camino::Utf8PathBuf;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 pub struct Args {
     /// SVN Server URL
-    #[arg(short, long)]
+    #[arg(short = 'u', long)]
     pub svn_url: Url,
+
+    /// SVN username
+    #[arg(short, long)]
+    pub svn_username: String,
+
+    /// Path to destination git repo
+    #[arg(short, long)]
+    pub destination_repo: Utf8PathBuf,
+
+    /// Committer name (Github name surname)
+    #[arg(short = 'n', long)]
+    pub committer_name: String,
+
+    /// Committer email address
+    #[arg(short = 'e', long)]
+    pub committer_email: EmailAddress,
 }
